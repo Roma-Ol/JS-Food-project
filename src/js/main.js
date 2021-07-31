0787f2520a5bbd330595176f0d7f9130e2dc501c
RomaRoma <v12nes11@gmail.com>
2021-07-31 22:08:54 +0300

    tsk-3 modal

--- src/js/main.js
+++ src/js/main.js
@@ -1,54 +1,137 @@
 "use strict";
 
 // Starting 2 load js after all the DOM tree loaded.
-document.addEventListener('DOMContentLoaded', ()=>{
+document.addEventListener("DOMContentLoaded", () => {
+    // TABS.
+    //
 
     // Variables we need.
-    const tabs = document.querySelectorAll('.tabheader__item'),
-          tabsContent = document.querySelectorAll('.tabcontent'),
-          tabsParent = document.querySelector('.tabheader__items');
+    const tabs = document.querySelectorAll(".tabheader__item"),
+        tabsContent = document.querySelectorAll(".tabcontent"),
+        tabsParent = document.querySelector(".tabheader__items");
 
     // Hide all the unnecessary tabs.
     function hideTabContent() {
-        tabsContent.forEach( item => {
-            item.style.display = 'none';
+        tabsContent.forEach(item => {
+            item.style.display = "none";
         });
 
         tabs.forEach(item => {
-            item.classList.remove('tabheader__item_active');
-        })
-    };
+            item.classList.remove("tabheader__item_active");
+        });
+    }
 
     // Show 1 necessary tab.
     function showTabContent(i = 0) {
-        tabsContent[i].style.display = 'block';
-        tabs[i].classList.add('tabheader__item_active');
-    };
+        tabsContent[i].style.display = "block";
+        tabs[i].classList.add("tabheader__item_active");
+    }
     hideTabContent();
     showTabContent();
 
     // Click event.
-    tabsParent.addEventListener('click', (event) => {
-        if (event.target && event.target.matches('div.tabheader__item')) {
-             tabs.forEach((item, i) => {
-                 if (event.target == item) {
-                     hideTabContent();
-                     showTabContent(i);
-                 }
-             })
+    tabsParent.addEventListener("click", (event) => {
+        if (event.target && event.target.matches("div.tabheader__item")) {
+            tabs.forEach((item, i) => {
+                if (event.target == item) {
+                    hideTabContent();
+                    showTabContent(i);
+                }
+            });
         }
     });
 
+    // TIMER.
 
+    const deadline = "2021-09-23";
 
+    function getTimeRemaining(endtime) {
+        const t = Date.parse(endtime) - Date.parse(new Date()),
+            days = Math.floor(t / (1000 * 60 * 60 * 24)),
+            hours = Math.floor((t / (1000 * 60 * 60) % 24)),
+            minuetes = Math.floor((t / 100 / 60) % 60),
+            seconds = Math.floor((t / 1000) % 60);
+        return {
+            "total": t,
+            "days": days,
+            "hours": hours,
+            "minutes": minuetes,
+            "seconds": seconds
+        };
+    }
 
+    function getZero(num) {
+        if (num >= 0 && num < 10) {
+            return (`0${num}`);
+        } else {
+            return num;
+        }
+    }
 
+    function setClock(selector, endtime) {
+        const timer = document.querySelector(selector),
+            days = timer.querySelector("#days"),
+            hours = timer.querySelector("#hours"),
+            minutes = timer.querySelector("#minutes"),
+            seconds = timer.querySelector("#seconds"),
+            timeInterval = setInterval(updateClock, 1000);
 
+        updateClock();
 
+        function updateClock() {
+            const t = getTimeRemaining(endtime);
 
+            days.innerHTML = getZero(t.days);
+            hours.innerHTML = getZero(t.hours);
+            minutes.innerHTML = getZero(t.minutes);
+            seconds.innerHTML = getZero(t.seconds);
 
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+            }
+        }
+    }
+    setClock(".timer", deadline);
 
+    // MODAL WINDOWS WITH FORM
 
+    // Getting the variables we'll use;
+    let allButtons = document.querySelectorAll("[data-modal]"),
+        modal = document.querySelector(".modal"),
+        close = modal.querySelector("[data-close]");
 
+    // Func 4 showing the form.
+    function showForm(element) {
+        element.classList.add("show");
+        element.classList.remove("hide");
+        document.body.style.overflow = "hidden";
+    }
 
+    // Function 4 hiding the form.
+    function hideForm(element) {
+        element.classList.add("hide");
+        element.classList.remove("show");
+        document.body.style.overflow = "";
+    }
+
+    // Showing the form on every button click.
+    allButtons.forEach(item => {
+        item.addEventListener("click", () => {
+          showForm(modal);
+        });
+    });
+
+    // Hiding the form on click over or click on X.
+    modal.addEventListener("click", (event)=> {
+        if(event.target === modal || event.target === close) {
+            hideForm(modal);
+        }
+    });
+
+    // Click Esc 2 hide the form.
+    document.addEventListener("keydown", (event)=>{
+        if (event.code === "Escape" && modal.classList.contains("show")) {
+            hideForm(modal);
+        }
+    });
 });
